fdb5f5479562c7ca19ac88b2fd0b710ef4043fc3
Wellber Gabriel <102326689+KrostterX6@users.noreply.github.com>
2025-12-09 21:47:35 -0300

    Add files via upload

---
+++ 2413.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    printf("%d\n", t * 4);
+
+    return 0;
+}
+
+//bom, você disse 30 exercicios, não especificou a dificuldade deles kkkkkkkk
